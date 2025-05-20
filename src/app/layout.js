e756940a42188d312f95679a71b130c5dcb85453
jaytillery <samurai@streetcode.dev>
2025-05-20 18:19:50 -0400

    Fix google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import Script from 'next/script';
-import AnalyticsListener from './providers';
+import { Providers } from './providers';
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
+        {/* GA script tags */}
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
@@ -45,8 +45,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <AnalyticsListener />
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,17 +1,17 @@
 'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { pageview } from '@/lib/gtag';
 
-export function AnalyticsListener() {
+export function Providers({ children }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = pathname + searchParams.toString();
+    const url = pathname + '?' + searchParams.toString();
     pageview(url);
   }, [pathname, searchParams]);
 
-  return null;
+  return children;
 }
