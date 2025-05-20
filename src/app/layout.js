0241a83b3ed88e091970ee8fa054bd9e2d256463
jaytillery <samurai@streetcode.dev>
2025-05-20 18:03:06 -0400

    Add Provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from 'next/script';
+import AnalyticsListener from './providers';
 import "./globals.css";
 
 const geistSans = Geist({
