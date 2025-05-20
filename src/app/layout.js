a9f2ca396932c936624ea338b7fe86400cfd0ece
jaytillery <samurai@streetcode.dev>
2025-05-20 18:34:54 -0400

    Update providers.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,3 @@
-'use client';
-
 import Script from 'next/script';
 import { Providers } from './providers';
 import { Geist, Geist_Mono } from "next/font/google";
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,17 +1,28 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { pageview } from '@/lib/gtag';
 
-export function Providers({ children }) {
+function AnalyticsTracker() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = pathname + '?' + searchParams.toString();
+    const url = `${pathname}?${searchParams.toString()}`;
     pageview(url);
   }, [pathname, searchParams]);
 
-  return children;
+  return null;
+}
+
+export function Providers({ children }) {
+  return (
+    <>
+      <Suspense fallback={null}>
+        <AnalyticsTracker />
+      </Suspense>
+      {children}
+    </>
+  );
 }
